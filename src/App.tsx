8364a5560cf83627479c6faf3a2761878e1e315e
unknown <majidkouki56@gmail.com>
2025-11-14 12:09:25 +0100

    Update logo and resume data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Hero from './components/Hero'
 import About from './components/About'
